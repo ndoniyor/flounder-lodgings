f52c17620057c5bb059532e3c570a674f0d3a9ce
Doniyor Nimatullo <47197048+ndoniyor@users.noreply.github.com>
2022-08-23 17:47:47 -0400

    added migrations

--- flounder_web/rooms/models.py
+++ flounder_web/rooms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    room_number = models.IntegerField()
+    bed_count = models.IntegerField()
+    price = models.IntegerField()
+    status = models.TextField()
+    checkout_time = models.DateField()
